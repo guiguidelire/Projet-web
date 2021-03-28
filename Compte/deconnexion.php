4a514ceed4e7be5efdfd694bd57c502c9a54b14a
LudoAW <ludovic.wittmann1@viacesi.fr>
2021-03-28 17:10:05 +0200

    Ajout poo Compte creation

--- Compte/deconnexion.php
+++ Compte/deconnexion.php
@@ -1,144 +1,20 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>CESI STAGE</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-        <link href="../Assets/Styles/style.css" rel="stylesheet">
-        
-    </head>
-    <body>
-    <header>
-        <nav class="navbar navbar-inverse navbar-fixed-top"> 
-            <div class="container-fluid">
-              <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="../index.php">CESI Stage</a>
-              </div>
-              <div class="collapse navbar-collapse" id="myNavbar">
-                <ul class="nav navbar-nav navbar-right">
-                  
-                  <li><a href="../index.php">Accueil</a></li>
-                  
-                  <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Offres de stage
-                    <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                      <li><a href="./OffresDeStage/rechercheOffre.php">Rechercher</a></li>
-                      <li><a href="./OffresDeStage/création.php">Création</a></li>
-                    </ul>
-                  </li>
-                  
-                  <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Entreprise
-                    <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                      <li><a href="./Entreprises/rechercher.php">Rechercher</a></li>
-                      <li><a href="./Entreprises/gerer.php">Gerer</a></li>
-                    </ul>
-                  </li>
-                  
-                  
-                  <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Compte
-                    <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                      <li><a href="./monCompte.php">Mon Compte</a></li>
-                      <?php 
-                        if(isset($_COOKIE['Login'])){ ?>
-                          <li style="background-color:#EE0000"><a href="./deconnexion.php">Deconnexion</a></li>
-                          <?php
-                        }
-                      ?>
-                    </ul>
-                  </li>
+<?php 
+require("../Assets/ConnexionBDD.php");//$conn : connexion bdd
+setcookie('Login', '', 1,"/");
+setcookie('ID_utilisateur', '', 1,"/");
+setcookie('Fonction', '', 1,"/");
+  
+$page = "deconnexion";
+require("../Nav/header.php");
+?>
 
-
-                  <?php if(isset($_COOKIE['Login'])){ 
-                    if($_COOKIE['Fonction']==1 || $_COOKIE['Fonction']==3){?>
-                      <li class="dropdown">
-                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Autre création et Aministration
-                        <span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                          <li><a href="./CréationCompte.php">Créer un nouveau compte</a></li>
-                        </ul>
-                      </li>
-                    <?php
-                    }
-                  }
-                ?>
-                </ul>
-              </div>
-            </div>
-          </nav>
-        </header>
 <!------------------------------------------------------------------------------------------------------------------------->
 
         <main>
-            <?php
-            $servername = 'localhost';
-            $username = 'root';
-            $password = '';
-            
-            try{
-                $conn = new PDO("mysql:host=$servername;dbname=bdd_cesi_stage", $username, $password);
-                //On définit le mode d'erreur de PDO sur Exception
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                //echo 'Connexion réussie <br> <br>';
-            }
-
-            //On capture les exceptions si une exception est lancée et on affiche
-            //les informations relatives à celle-ci
-            catch(PDOException $e){
-                echo "Erreur : " . $e->getMessage() . "<br>";
-            }
-
-
-            setcookie('Login', '', 1,"/");
-            setcookie('ID_utilisateur', '', 1,"/");
-            setcookie('Fonction', '', 1,"/");
-
-                    
-            ?>
             <div class="container">
                 <p> Vous venez de vous deconnecter.</p>
             </div>
           </main>
 <!------------------------------------------------------------------------------------------------------------------------->
-        <footer class="text-center text-lg-start">
-          <div class="container p-4">
-            <div class="row">
-              <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-                <h5 class="text-uppercase">Footer Content</h5>
 
-                <p>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
-                  molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam
-                  voluptatem veniam, est atque cumque eum delectus sint!
-                </p>
-              </div>
-
-              <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-                <h5 class="text-uppercase">Links</h5>
-
-                <ul class="list-unstyled mb-0">
-                  <li>
-                    <a href="https://wayf.cesi.fr/login" class="text-dark">Ent CESI</a>
-                  </li>
-                  <li>
-                    <a href="https://www.cesi.fr/" class="text-dark">CESI.fr</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </footer>
-    </body>
-</html>
+<?php require("../Nav/footer.php"); ?>
