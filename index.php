dfdc0d76919fd674858a32ad453fa480550aa281
Guillaume <guillaume.menetre@viacesi.fr>
2021-03-29 00:20:20 +0200

    fix bug carousel

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
     <head>
         <title>CESI STAGE</title>
@@ -8,7 +8,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
         <link href="./Assets/Styles/style.css" rel="stylesheet">
-        
+           
     </head>
     <body>
         <header>
